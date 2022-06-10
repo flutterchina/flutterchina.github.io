45cae8c6adaecb6340beea0106d1a00560bc3eba
wendux <824783146@qq.com>
2022-06-10 10:52:44 +0800

    Trigger rebuild

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # git subtree push --prefix docs web gh-pages
-cd _site
-tcb hosting deploy . -e flutter-tg-2g8uxssl2f403089 -r gz
+
+git commit --allow-empty -m "Trigger rebuild"
+git push web master -f
+cd docs
+tcb hosting deploy . -e flutter-6gh3is35685ac8b9 -r gz
